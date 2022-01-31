861ea714881e75135b43a21362812bb870f451ec
Kris <dauthi66@gmail.com>
2022-01-30 19:17:05 -0800

    Added DBContext class

---
+++ ECommerceSite/Data/CrateContext.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceSite.Data
+{
+    public class CrateContext : DbContext
+    {
+        //constructor for database, inherit DbcontextOptions, argument options
+        public CrateContext(DbContextOptions<CrateContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Crate> Crates { get; set; }
+    }
+}
