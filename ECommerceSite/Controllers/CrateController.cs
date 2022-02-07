3028a8a67b0332aef8f61d96740d8f4cdf41be4b
Kris <dauthi66@gmail.com>
2022-02-06 17:26:24 -0800

    made a create game view

---
+++ ECommerceSite/Controllers/CrateController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceSite.Controllers
+{
+    public class CrateController : Controller
+    {
+        //displays page to user. To add razor view click on create()
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
