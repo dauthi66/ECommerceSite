415eed58401c483968233aabdf82fa7a21fea07f
Kris <dauthi66@gmail.com>
2022-03-12 13:59:00 -0800

    fixed a null warning in index.  Added shoping cart button to game catalog.

---
+++ ECommerceSite/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using ECommerceSite.Data;
+using ECommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CrateContext _crateContext;
+
+        public CartController(CrateContext crateContext)
+        {
+            _crateContext = crateContext;
+        }
+
+        //ID used as identifier for asp.net here and in controller
+        public IActionResult Add(int id)
+        {
+            Crate? crateToAdd = _crateContext.Crates.Where(c => c.CrateId == id).SingleOrDefault();
+
+            if (crateToAdd == null)
+            {
+                TempData["Message"] = "Sorry that game no longer exists.";
+                return RedirectToAction("Index", "Crate");
+            }
+            //TODO: add item to shopping cart cookie
+            TempData["Message"] = "Crate successfully added to cart!";
+            return RedirectToAction("Index", "Crate");
+        }
+    }
+}
